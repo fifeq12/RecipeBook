5c843cf6b2b04b1307d3f396a429a9a6b98180c0
fifeq12 <filip.wajs99@gmail.com>
2020-09-07 14:02:53 +0200

    Dodaj pliki projektów.

---
+++ Models/Recipe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public int PreparationTime { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        [EnumDataType(typeof(IngredientsType))]
+        public IngredientsType IngredientsType { get; set; }
+        [Required]
+        [EnumDataType(typeof(Difficulty))]
+        public Difficulty Difficulty { get; set; }
+        [Required]
+        public int RecipeTypeId { get; set; }
+        [ForeignKey("RecipeTypeId")]
+        public RecipeType RecipeType { get; set; }
+    }
+    public enum IngredientsType
+    {
+        Mięsne,
+        Wegetariańskie,
+        Wegańskie
+    }
+    public enum Difficulty
+    {
+        Łatwe,
+        Średnie,
+        Trudne
+    }
+}
---
+++ Models/RecipeType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class RecipeType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+    }
+}
